8aaa433a834a5fc79ee5ee49bd813c861d2c5df7
Isuru Hettiarachchi <isuru71@gmail.com>
2019-04-11 02:32:17 +0530

    Add time picker and ringtone picker

--- app/src/main/java/com/ctse/alarmsystem/MainActivity.java
+++ app/src/main/java/com/ctse/alarmsystem/MainActivity.java
@@ -1,13 +1,71 @@
 package com.ctse.alarmsystem;
 
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+    FloatingActionButton addAlarmButton;
+    Button setRingtoneButton;
+    Uri ringtoneUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addAlarmButton = findViewById(R.id.addAlarmButton);
+        setRingtoneButton = findViewById(R.id.setRingtoneButton);
+
+        addAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+
+            }
+        });
+
+        setRingtoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALARM);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView alarmTime = findViewById(R.id.alarmTimeTextView);
+        alarmTime.setText(hourOfDay + ":" + minute);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                ringtoneUri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+                Ringtone ringtone = RingtoneManager.getRingtone(this, ringtoneUri);
+                String ringtoneName = ringtone.getTitle(this);
+                setRingtoneButton.setText(ringtoneName);
+                Log.i("ringone", ringtoneUri.toString());
+                Log.i("ringtone name", ringtoneName);
+            }
+        }
     }
 }
