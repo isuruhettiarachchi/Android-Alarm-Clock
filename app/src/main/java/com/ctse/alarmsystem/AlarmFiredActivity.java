22969f09e07df60f05d868c29c020870dc58a336
Isuru Hettiarachchi <isuru71@gmail.com>
2019-04-12 18:55:21 +0530

    Add fullscreen view to alarm dismiss activity

--- app/src/main/java/com/ctse/alarmsystem/AlarmFiredActivity.java
+++ app/src/main/java/com/ctse/alarmsystem/AlarmFiredActivity.java
@@ -29,6 +29,18 @@ public class AlarmFiredActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_fired);
 
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
 //        String ringtone = getIntent().getExtras().getString("ringtone");
 //
 //        Uri ringtoneUri = Uri.parse(ringtone);
